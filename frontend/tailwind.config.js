acae98c389015bb25366184f1d2cb12f534bbaae
Ilavf <ila990474@gmail.com>
2025-04-16 14:44:06 +0700

    chore: install DaisyUI untuk komponen UI

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      "./src/**/*.{html,js,jsx,ts,tsx}", // Sesuaikan dengan struktur file proyekmu
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [
+      require('daisyui'), // Menambahkan DaisyUI
+    ],
+  }
+  
